9376b60373ab837f22fb43ef7fcff2671d7a5121
wsaunders1014 <willsaunders1014@gmail.com>
2020-06-12 22:50:43 -0700

    added a Settings Menu, door icons are now always visible unless disabled in settings

--- scripts/shut-doors.js
+++ scripts/shut-doors.js
@@ -1,16 +1,33 @@
+/**
+ * These hooks register the following settings in the module settings.
+ */
+Hooks.once('init', () => {
 
-//Credit to Winks' Everybody Look Here for the code to add to Scene Nav
-function getContextOption2(idField) {
-    return {
-        name: "Reset Doors & Fog",
-        icon: '<i class="fas fa-dungeon"></i>',
-        condition: li => game.user.isGM,
-        callback: li => {
-            let scene = game.scenes.get(li.data(idField));      
-            resetDoorsAndFog(scene)
-        }
-    };
+  game.settings.register('ResetDoorsAndFog', 'forceDoorIcons', {
+    name: "reset-doors.force-doors-s",
+    hint: "reset-doors.force-doors-l",
+    scope: "world",
+    config: true,
+    default: true,
+    type: Boolean,
+    onChange: x => window.location.reload()
+  });
+
+});
+
+//Overwrite Activate call to prevent doors from going invisible.
+WallsLayer.prototype.activate = function(){
+	console.log('Settings:',game.settings.get('ResetDoorsAndFog','forceDoorIcons'))
+	
+  PlaceablesLayer.prototype.activate.call(this)
+  //Force Show Doors is set to True
+  if(game.settings.get('ResetDoorsAndFog','forceDoorIcons')===false){
+  	if (canvas.controls) canvas.controls.doors.visible = false;
+
+  }
+   
 }
+
 //Just a parent function for both sub functions. Kept functionality separate in case I want to detangle them later.
 async function resetDoorsAndFog(scene){
 	let isCurrentScene = scene.data._id == canvas.scene.data._id;
@@ -44,6 +61,18 @@ async function resetFog(isCurrentScene,id=null){
 	}
 	
 }
+//Credit to Winks' Everybody Look Here for the code to add menu option to Scene Nav
+function getContextOption2(idField) {
+    return {
+        name: "Reset Doors & Fog",
+        icon: '<i class="fas fa-dungeon"></i>',
+        condition: li => game.user.isGM,
+        callback: li => {
+            let scene = game.scenes.get(li.data(idField));      
+            resetDoorsAndFog(scene)
+        }
+    };
+}
 //Adds menu option to Scene Nav and Directory
 Hooks.on("getSceneNavigationContext", (html, contextOptions) => {
     contextOptions.push(getContextOption2('sceneId'));
@@ -67,5 +96,36 @@ Hooks.on("getSceneControlButtons", function(controls){
     })
     return controls;
 	
+})
+
+Hooks.on('ready', function(){
+  if(game.settings.get('ResetDoorsAndFog','forceDoorIcons')===true){
+  	/**
+   	* Override foundry's ControlsLayer.drawDoors function in order to prevent Door Controls from being deleted.
+ 	*/
+	ControlsLayer.prototype.drawDoors = function() {
+	    console.log('my drawDoors')
+	    // Create the container
+	    if ( this.doors ) {
+	      this.doors.destroy({children: true});
+	      this.doors = null;
+	    }
+	    const doors = new PIXI.Container();
+
+	    // Iterate over all walls, selecting the doors
+	    for ( let w of canvas.walls.placeables ) {
+	      if ( w.data.door === CONST.WALL_DOOR_TYPES.NONE ) continue;
+	      if ( (w.data.door === CONST.WALL_DOOR_TYPES.SECRET) && !game.user.isGM ) continue;
+	      let dc = doors.addChild(new DoorControl(w));
+	      //dc.visible = false; // Start door controls as initially not visible and reveal them later
+	      dc.draw();
+	    }
+	    this.doors = this.addChild(doors);
+
+	    // Toggle visibility for the set of door control icons
+	   // this.doors.visible = !canvas.walls._active;
+	  }
+	}
+
 })
 
