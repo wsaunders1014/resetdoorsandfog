f305d54f02e4b045818f0d9fe95cf063eec9a00e
wsaunders1014 <willsaunders1014@gmail.com>
2020-06-16 21:38:44 -0700

    Separate functionality into other modules.

--- scripts/resetdoorsandfog.js
+++ scripts/resetdoorsandfog.js
@@ -1,41 +1,19 @@
-/**
- * These hooks register the following settings in the module settings.
- */
-Hooks.once('init', () => {
 
-  game.settings.register('ResetDoorsAndFog', 'forceDoorIcons', {
-    name: "reset-doors.force-doors-s",
-    hint: "reset-doors.force-doors-l",
-    scope: "world",
-    config: true,
-    default: true,
-    type: Boolean,
-    onChange: x => window.location.reload()
-  });
+/******************************************************************************************
 
-});
 
-//Overwrite Activate call to prevent doors from going invisible.
-WallsLayer.prototype.activate = function(){
-	console.log('Settings:',game.settings.get('ResetDoorsAndFog','forceDoorIcons'))
-	
-  PlaceablesLayer.prototype.activate.call(this)
-  //Force Show Doors is set to True
-  if(game.settings.get('ResetDoorsAndFog','forceDoorIcons')===false){
-  	if (canvas.controls) canvas.controls.doors.visible = false;
+RESET DOORS AND FOG BUTTONS
 
-  }
-   
-}
 
+******************************************************************************************/
 //Just a parent function for both sub functions. Kept functionality separate in case I want to detangle them later.
 async function resetDoorsAndFog(scene){
 	let isCurrentScene = scene.data._id == canvas.scene.data._id;
 	await resetDoors(isCurrentScene,scene.data._id);
 	await resetFog(isCurrentScene,scene.data._id);
 }
-/******** RESET DOOR **************
-**********************************/
+/******** RESET DOOR **************/
+
 async function resetDoors(isCurrentScene,id=null){
 	if(isCurrentScene){
 		await canvas.walls.doors.forEach((item)=> item.update({ds:0}));
@@ -45,8 +23,11 @@ async function resetDoors(isCurrentScene,id=null){
 	}
 	ui.notifications.info(`Doors have been shut.`);
 }
-/******** RESET FOG **************
-**********************************/
+
+/**********************************/
+
+/******** RESET FOG **************/
+
 async function resetFog(isCurrentScene,id=null){
 	if(isCurrentScene){
 		canvas.sight.resetFog();
@@ -61,6 +42,7 @@ async function resetFog(isCurrentScene,id=null){
 	}
 	
 }
+/**********************************/
 //Credit to Winks' Everybody Look Here for the code to add menu option to Scene Nav
 function getContextOption2(idField) {
     return {
@@ -90,42 +72,28 @@ Hooks.on("getSceneControlButtons", function(controls){
       title: "Close All Doors",
       icon: "fas fa-door-closed",
       onClick: () => {
-		resetDoors(true);
-	  },
+		    resetDoors(true);
+	    },
       button: true
-    })
-    return controls;
+  })
+  return controls;
 	
 })
 
-Hooks.on('ready', function(){
-  if(game.settings.get('ResetDoorsAndFog','forceDoorIcons')===true){
-  	/**
-   	* Override foundry's ControlsLayer.drawDoors function in order to prevent Door Controls from being deleted.
- 	*/
-	ControlsLayer.prototype.drawDoors = function() {
-	    console.log('my drawDoors')
-	    // Create the container
-	    if ( this.doors ) {
-	      this.doors.destroy({children: true});
-	      this.doors = null;
-	    }
-	    const doors = new PIXI.Container();
 
-	    // Iterate over all walls, selecting the doors
-	    for ( let w of canvas.walls.placeables ) {
-	      if ( w.data.door === CONST.WALL_DOOR_TYPES.NONE ) continue;
-	      if ( (w.data.door === CONST.WALL_DOOR_TYPES.SECRET) && !game.user.isGM ) continue;
-	      let dc = doors.addChild(new DoorControl(w));
-	      //dc.visible = false; // Start door controls as initially not visible and reveal them later
-	      dc.draw();
-	    }
-	    this.doors = this.addChild(doors);
+/*********************************************************************************/
 
-	    // Toggle visibility for the set of door control icons
-	   // this.doors.visible = !canvas.walls._active;
-	  }
-	}
 
-})
 
+
+Hooks.once('init', ()=>{
+    game.settings.register('resetdoorsandfog', 'enabled', {
+      name: "rsdaf.enable-s",
+      hint: "rsdaf.enable-l",
+      scope: "world",
+      config: true,
+      default: true,
+      type: Boolean,
+      onChange: x => window.location.reload()
+    });
+});
