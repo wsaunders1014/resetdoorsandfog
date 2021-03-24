2f31cd1958b9c99eb20771ff307cda1820dd28a8
yacklebeam <jacob@jmtroxel.io>
2021-03-23 18:13:55 -0700

    Updated JS to only close OPEN doors

--- scripts/resetdoorsandfog.js
+++ scripts/resetdoorsandfog.js
@@ -5,23 +5,23 @@
 RESET DOORS AND FOG BUTTONS
 
 
-******************************************************************************************/
+ ******************************************************************************************/
 //Just a parent function for both sub functions. Kept functionality separate in case I want to detangle them later.
 async function resetDoorsAndFog(scene){
-	let isCurrentScene = scene.data._id == canvas.scene.data._id;
-	await resetDoors(isCurrentScene,scene.data._id);
-	await resetFog(isCurrentScene,scene.data._id);
+    let isCurrentScene = scene.data._id == canvas.scene.data._id;
+    await resetDoors(isCurrentScene,scene.data._id);
+    await resetFog(isCurrentScene,scene.data._id);
 }
 /******** RESET DOOR **************/
 
 async function resetDoors(isCurrentScene,id=null){
-	if(isCurrentScene){
-		await canvas.walls.doors.forEach((item)=> item.update({ds:0}));
-	}else{
-		console.log(game.scenes.get(id).data.walls.filter((item)=> item.door != 0))
-		await game.scenes.get(id).data.walls.filter((item)=> item.door != 0).forEach((x) => x.ds = 0);
-	}
-	ui.notifications.info(`Doors have been shut.`);
+    if(isCurrentScene){
+        await canvas.walls.doors.filter((item) => item.data.ds == 1).forEach((item)=> item.update({ds:0}));
+    }else{
+        console.log(game.scenes.get(id).data.walls.filter((item)=> item.door != 0))
+        await game.scenes.get(id).data.walls.filter((item)=> item.door != 0).forEach((x) => x.ds = 0);
+    }
+    ui.notifications.info(`Doors have been shut.`);
 }
 
 /**********************************/
@@ -29,18 +29,18 @@ async function resetDoors(isCurrentScene,id=null){
 /******** RESET FOG **************/
 
 async function resetFog(isCurrentScene,id=null){
-	if(isCurrentScene){
-		canvas.sight.resetFog();
-	}else{
-		 const response = await SocketInterface.dispatch("modifyDocument", {
-	      type: "FogExploration",
-	      action: "delete",
-	      data: {scene:id},
-	      options: {reset: true}
-	    });
-		ui.notifications.info(`Fog of War exploration progress was reset.`);
-	}
-	
+    if(isCurrentScene){
+        canvas.sight.resetFog();
+    }else{
+        const response = await SocketInterface.dispatch("modifyDocument", {
+            type: "FogExploration",
+            action: "delete",
+            data: {scene:id},
+            options: {reset: true}
+        });
+        ui.notifications.info(`Fog of War exploration progress was reset.`);
+    }
+
 }
 /**********************************/
 //Credit to Winks' Everybody Look Here for the code to add menu option to Scene Nav
@@ -50,7 +50,7 @@ function getContextOption2(idField) {
         icon: '<i class="fas fa-dungeon"></i>',
         condition: li => game.user.isGM,
         callback: li => {
-            let scene = game.scenes.get(li.data(idField));      
+            let scene = game.scenes.get(li.data(idField));
             resetDoorsAndFog(scene)
         }
     };
@@ -66,18 +66,18 @@ Hooks.on("getSceneDirectoryEntryContext", (html, contextOptions) => {
 
 //Adds Shut All Doors button to Walls Control Layer
 Hooks.on("getSceneControlButtons", function(controls){
-		
-	controls[4].tools.splice(controls[4].tools.length-2,0,{
-      name: "close",
-      title: "Close All Doors",
-      icon: "fas fa-door-closed",
-      onClick: () => {
-		    resetDoors(true);
-	    },
-      button: true
-  })
-  return controls;
-	
+
+    controls[4].tools.splice(controls[4].tools.length-2,0,{
+        name: "close",
+        title: "Close Open Doors",
+        icon: "fas fa-door-closed",
+        onClick: () => {
+            resetDoors(true);
+        },
+        button: true
+    })
+    return controls;
+
 })
 
 
